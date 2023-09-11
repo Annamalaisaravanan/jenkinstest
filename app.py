f00129b961e63c386e9a1ba8d62438818bd551f5
Annamalai <Annamalai@course5i.com>
2023-09-11 21:42:43 +0530

    new aws keys

--- app.py
+++ app.py
@@ -6,19 +6,21 @@ import pandas as pd
 access_key = os.environ.get('access_key')
 secret_key = os.environ.get('secret_key')
 
-s3 = boto3.resource("s3",aws_access_key_id=access_key, 
-                      aws_secret_access_key=secret_key, 
-                      region_name='ap-south-1')
+print(access_key,secret_key)
+
+# s3 = boto3.resource("s3",aws_access_key_id=access_key, 
+#                       aws_secret_access_key=secret_key, 
+#                       region_name='ap-south-1')
 
 
-s3_object_key = 'CVD_cleaned.csv'
+# s3_object_key = 'CVD_cleaned.csv'
 
-s3_object = s3.Object('mlflow-artifacts-anna', s3_object_key)
+# s3_object = s3.Object('mlflow-artifacts-anna', s3_object_key)
 
-print(s3_object)
+# print(s3_object)
                 
-csv_content = s3_object.get()['Body'].read()
+# csv_content = s3_object.get()['Body'].read()
 
-df_input = pd.read_csv(BytesIO(csv_content))
+# df_input = pd.read_csv(BytesIO(csv_content))
 
-print(df_input.shape)
+# print(df_input.shape)

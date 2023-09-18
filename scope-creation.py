7d50ded5bc4a398ab100c2f5de7bca7332a739e5
Annamalai <Annamalai@course5i.com>
2023-09-18 15:32:47 +0530

    2 arg session

--- scope-creation.py
+++ scope-creation.py
@@ -42,7 +42,7 @@ headers = {
 session = requests.Session()
 
 scopes_list =  session.get('https://dbc-da2540cb-9415.cloud.databricks.com/scopes/list',headers=headers)
-print(scopes_list.json())
+print(scopes_list)
         
 scope_name = []
 for scope in scopes_list['scopes']:

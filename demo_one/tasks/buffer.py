c4bf0bbce6b7fbb6473d682b510683b2d2d6cfe3
Annamalai <Annamalai@course5i.com>
2023-09-26 12:18:36 +0530

    utils.py added

--- demo_one/tasks/buffer.py
+++ demo_one/tasks/buffer.py
@@ -31,10 +31,18 @@ import mlflow
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, r2_score, auc, roc_curve
 
+import random
+import string
 
+def random_string(base_string):
+       
 
-from demo_one.utils import random_string
-from demo_one.utils import push_df_to_s3, mlflow_call_endpoint, read_data_from_s3, read_secrets
+        # Generate a random number (assuming you want a 6-digit number)
+        random_number = ''.join(random.choices(string.digits, k=6))
+
+        result_string = base_string + "_" + random_number
+
+        return result_string
 
 
 
@@ -44,9 +52,10 @@ with open('config.yml', 'r') as file:
 
 from mlflow.utils.rest_utils import http_request
 import json
-
 def Client():
   return mlflow.tracking.MlflowClient()
+
+
 
 warnings.filterwarnings('ignore')
 
@@ -58,14 +67,39 @@ spark = SparkSession.builder.appName("CSV Loading Example").getOrCreate()
 
 dbutils = DBUtils(spark)
 
-# aws_access_key = dbutils.secrets.get(scope="anna-scope", key="aws_access_key")
-# aws_secret_key = dbutils.secrets.get(scope="anna-scope", key="aws_secret_key")
-# db_token = dbutils.secrets.get(scope="anna-scope", key="databricks-token")
+aws_access_key = dbutils.secrets.get(scope="anna-scope", key="aws_access_key")
+aws_secret_key = dbutils.secrets.get(scope="anna-scope", key="aws_secret_key")
 
-aws_access_key, aws_secret_key, db_token = read_secrets(dbutils,'anna-scope',['aws_access_key','aws_secret_key','databricks-token'])
+
+access_key = aws_access_key
+secret_key = aws_secret_key
 
 
 class DataPrep(Task):
+
+    def push_df_to_s3(self,df,s3_object_key,access_key,secret_key):
+            csv_buffer = BytesIO()
+            df.to_csv(csv_buffer, index=False)
+            csv_content = csv_buffer.getvalue()
+
+            s3 = boto3.resource("s3",aws_access_key_id=access_key, 
+                      aws_secret_access_key=secret_key, 
+                      region_name='ap-south-1')
+
+            
+            s3.Object(configure['s3']['bucket_name'], s3_object_key).put(Body=csv_content)
+
+            return {"df_push_status": 'successs'}
+    
+    def mlflow_call_endpoint(self, endpoint, method, body='{}'):
+                if method == 'GET':
+                    response = http_request(
+                        host_creds=host_creds, endpoint="/api/2.0/mlflow/{}".format(endpoint), method=method, params=json.loads(body))
+                else:
+                    response = http_request(
+                        host_creds=host_creds, endpoint="/api/2.0/mlflow/{}".format(endpoint), method=method, 
+                        json=json.loads(body))
+                return response.json()
     
     def load_data(self, table_name, lookup_key,target, inference_data_df):
                     # In the FeatureLookup, if you do not provide the `feature_names` parameter, all features except primary keys are returned
@@ -100,6 +134,8 @@ class DataPrep(Task):
                                 LR_Classifier.fit(X_train, y_train)
                                 y_pred = LR_Classifier.predict(X_test)
                         
+                                #mlflow.log_metric("test_mse", mean_squared_error(y_test, y_pred))
+                                #mlflow.log_metric("test_r2_score", r2_score(y_test, y_pred))
                                 fpr, tpr, threshold = roc_curve(y_test,y_pred)
                                 roc_auc = auc(fpr, tpr)
                                 f1_train = f1_score(y_test,y_pred)
@@ -116,6 +152,25 @@ class DataPrep(Task):
     
     def Model(self,df_input):
                 
+                
+
+                
+                # s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
+                #       aws_secret_access_key=aws_secret_key, 
+                #       region_name='ap-south-1')
+                
+                # bucket_name =  configure['s3']['bucket_name']
+                # csv_file_key = configure['preprocessed']['preprocessed_df_path']
+
+                
+                # s3_object = s3.Object(bucket_name, csv_file_key)
+                
+                # csv_content = s3_object.get()['Body'].read()
+
+                # df_input = pd.read_csv(BytesIO(csv_content))
+
+                
+
                 df_input_spark = spark.createDataFrame(df_input)
 
                 inference_data_df = df_input_spark.select(configure['feature-store']['lookup_key'], configure['features']['target'])
@@ -123,7 +178,15 @@ class DataPrep(Task):
                 X_train, X_test, y_train, y_test, X_val, y_val, training_set = self.load_data(configure['feature-store']['table_name'], configure['feature-store']['lookup_key'],configure['features']['target'],inference_data_df)
         
                 client = MlflowClient()
-               
+            
+ 
+                # try:
+                #      client.delete_registered_model(configure['Model_registry_name']) # Delete the model if already created
+                # except:
+                #      None
+
+                
+                
                 self.train_model(X_train, X_val, y_train, y_val, training_set, fs,client)
 
                 rows = X_test.count()[0]
@@ -149,15 +212,25 @@ class DataPrep(Task):
 
                 print(accuracy_score(output_df['prediction'],output_df['actual'])*100)
 
+
+                #self.push_df_to_s3(X_test,configure['preprocessed']['x_test'],aws_access_key,aws_secret_key)
+
+                #self.push_df_to_s3(y_test,configure['preprocessed']['y_test'],aws_access_key,aws_secret_key)
+
+
                 print("Model training is done")
 
     def webhook(self):
+
+        
+
+        db_token = dbutils.secrets.get(scope="anna-scope", key="databricks-token")
 
         lists = {
             "model_name":"pharma_model",
             "events": "MODEL_VERSION_TRANSITIONED_TO_PRODUCTION"
         }
-        js_list_res = mlflow_call_endpoint(host_creds,'registry-webhooks/list', 'GET', json.dumps(lists))
+        js_list_res = self.mlflow_call_endpoint('registry-webhooks/list', 'GET', json.dumps(lists))
 
         print(js_list_res)
         if js_list_res:
@@ -179,31 +252,117 @@ class DataPrep(Task):
                                 }
 
                 job_json= json.dumps(diction)
-                js_res = mlflow_call_endpoint(host_creds,'registry-webhooks/create', 'POST', job_json)
+                js_res = self.mlflow_call_endpoint('registry-webhooks/create', 'POST', job_json)
                 print(js_res)
 
                 print("Webhook Created for deployment job")
 
     def _preprocess_data(self):
 
-        s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
+
+        
+                # feature_df = new_df.toPandas()
+                # print("Feature store is already there")
+
+                # s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
+                #       aws_secret_access_key=aws_secret_key, 
+                #       region_name='ap-south-1')
+                
+                # bucket_name =  configure['s3']['bucket_name']
+                # csv_file_key = configure['s3']['file_path']
+
+                # s3_object = s3.Object(bucket_name, csv_file_key)
+                
+                # target_data = s3_object.get()['Body'].read() # target with lookup key
+
+                # combined_data = pd.merge(feature_df, target_data, on='customer_id')
+
+                # return combined_data
+
+        try:
+                #fs.get_table(f"{configure['feature-store']['table_name']}")
+                print("reading feature store")
+                new_df = fs.read_table(configure['feature-store']['table_name'])
+                print("new_df read from feature store")  
+
+                s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
                 aws_secret_access_key=aws_secret_key, 
                 region_name='ap-south-1')
-        try:
+                print("s3 client")
                 
-                new_df = fs.read_table(configure['feature-store']['table_name'])
+                bucket_name =  configure['s3']['bucket_name']
+                csv_file_key = configure['preprocessed']['preprocessed_df_path']
 
-                df_input = read_data_from_s3(s3,configure['s3']['bucket_name'],configure['preprocessed']['preprocessed_df_path'])
+                print(bucket_name,csv_file_key)
+
+                s3_object = s3.Object(bucket_name, csv_file_key)
+
+                print('s3 object')
+                
+                csv_content = s3_object.get()['Body'].read()
+
+                print('csv_constant')
+
+                df_input = pd.read_csv(BytesIO(csv_content))
+
+                print('df_input')    
 
                 return df_input 
         except:
+                print("Hiii changes detected")
+                print(f"Access key and secret key are {access_key} and {secret_key}")
 
-                df_input = read_data_from_s3(s3,configure['s3']['bucket_name'],configure['s3']['file_path'])
             
+                
+                encoded_secret_key = urllib.parse.quote(secret_key,safe="")
+
+                # bucket_name = configure['s3']['bucket_name']
+                # mount_name = configure['dbfs']['mount_name']
+
+                # url = 's3a://%s:%s@%s' %(access_key, encoded_secret_key, bucket_name)
+
+                # try:
+                #        dbutils.fs.unmount(mount_name)
+                
+                # except:
+                #         print("no s3 is mounted....")
+
+                # try:
+                       
+                #        dbutils.fs.mount(url,mount_name)
+                #        print("try is executed")
+                
+                # except Exception as e: 
+                #         print(f"the error is {e}")
+                #         pass
+
+
+                
+                s3 = boto3.resource("s3",aws_access_key_id=aws_access_key, 
+                      aws_secret_access_key=aws_secret_key, 
+                      region_name='ap-south-1')
+                
+                bucket_name =  configure['s3']['bucket_name']
+                csv_file_key = configure['s3']['file_path']
+
+                s3_object = s3.Object(bucket_name, csv_file_key)
+                
+                csv_content = s3_object.get()['Body'].read()
+
+                df_input = pd.read_csv(BytesIO(csv_content))
+
+                # spark_data = spark.read.format('csv')\
+                #   .option('header','true')\
+                #   .option('inferschema','true')\
+                #   .load(configure['dbfs']['file_name'])
+                
+                # df_input = spark_data.toPandas()
+
                 df_input = df_input.reset_index()
         
                 numerical_cols = configure['features']['numerical_cols']
                 
+
                 categorical_cols = configure['features']['categorical_cols']
 
                 df_encoded = df_input.copy()
@@ -230,7 +389,10 @@ class DataPrep(Task):
                 
                 pickled_data = pickle.dumps(encoders_dict)
                 pkl_path = configure['preprocessed']['encoders_path']
-                
+                #s3.Object(bucket_name, pkl_path).put(Body=pickled_data)
+                # push_status = self.push_df_to_s3(df_input)
+                # print(push_status)
+
                 df_input.rename(columns = {'index':'PATIENT_ID','Height_(cm)':'Height','Weight_(kg)':'Weight',
                 'Diabetes_No, pre-diabetes or borderline diabetes':'Diabetes_No_pre-diabetes_or_borderline_diabetes',
                 'Diabetes_Yes, but female told only during pregnancy':'Diabetes_Yes_but_female_told_only_during_pregnancy'}, inplace = True)
@@ -246,6 +408,8 @@ class DataPrep(Task):
 
                 df_spark = spark.createDataFrame(df_feature)
 
+                
+
                 fs.create_table(
                         name=table_name,
                         primary_keys=[configure['feature-store']['lookup_key']],
@@ -254,8 +418,20 @@ class DataPrep(Task):
                         description="health features"
                     )
                 
-                push_status = push_df_to_s3(df_input,configure['s3']['bucket_name'],configure['preprocessed']['preprocessed_df_path'],s3)
-                print(push_status)
+
+               
+                
+                #push_status = self.push_df_to_s3(df_input,configure['preprocessed']['preprocessed_df_path'],access_key,secret_key)
+                #print(push_status)
+
+                
+
+                #df_input_spark = spark.createDataFrame(df_input)
+
+                # #inference_data_df = df_input_spark.select("PATIENT_ID", "Heart_Disease_Yes")
+
+                # X_train, X_test, y_train, y_test, X_val, y_val, training_set = self.load_data(table_name, "PATIENT_ID",inference_data_df)
+
 
                 print("Feature Store is created")
 
@@ -271,7 +447,16 @@ class DataPrep(Task):
                 return df_input
                 
 
-            
+                
+
+                   
+             
+
+
+
+                
+  
+
 
     def launch(self):
          
---
+++ demo_one/utils.py
@@ -0,0 +1,57 @@
+import random
+import string
+from io import BytesIO
+import boto3
+import pandas as pd
+import json
+from mlflow.utils.rest_utils import http_request
+
+
+def random_string(base_string):
+        # Generate a random number (assuming you want a 6-digit number)
+        random_number = ''.join(random.choices(string.digits, k=6))
+
+        result_string = base_string + "_" + random_number
+
+        return result_string
+
+
+def push_df_to_s3(df,bucket_name,object_key,s3):
+            csv_buffer = BytesIO()
+            df.to_csv(csv_buffer, index=False)
+            csv_content = csv_buffer.getvalue()
+            
+            s3.Object(bucket_name, object_key).put(Body=csv_content)
+
+            return {"df_push_status": 'successs'}
+
+
+def mlflow_call_endpoint(host_cred, endpoint, method, body='{}'):
+                if method == 'GET':
+                    response = http_request(
+                        host_creds=host_cred, endpoint="/api/2.0/mlflow/{}".format(endpoint), method=method, params=json.loads(body))
+                else:
+                    response = http_request(
+                        host_creds=host_cred, endpoint="/api/2.0/mlflow/{}".format(endpoint), method=method, 
+                        json=json.loads(body))
+                return response.json()
+
+def read_data_from_s3(s3,bucket_name, csv_file_key):
+        
+        s3_object = s3.Object(bucket_name, csv_file_key)
+
+        csv_content = s3_object.get()['Body'].read()
+
+        df_input = pd.read_csv(BytesIO(csv_content))
+
+        return df_input  
+
+
+def read_secrets(dbutils,scope,keys):
+        
+        h=tuple()
+        for key in keys:
+             j = dbutils.secrets.get(scope=scope, key=key)
+             h = h+ (j,)
+        return j
+
